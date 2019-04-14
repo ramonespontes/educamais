0b4b1a098b802ae8e9fbc7206e15374cffa2e787
ramon <ramonespontes@gmail.com>
2019-04-14 11:03:31 -0300

    Ajuste Layout Chamada a tela de Menu Chamada a tela de cadastro

--- app/src/main/java/ifpb/edu/br/educamais/MainActivity.java
+++ app/src/main/java/ifpb/edu/br/educamais/MainActivity.java
@@ -1,13 +1,45 @@
 package ifpb.edu.br.educamais;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editEmail;
+    private EditText editPassword;
+    private Button buttonEntrar;
+    private Button buttonCadastrarUsuario;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editEmail = (EditText) findViewById(R.id.editTextEmail);
+        editPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonEntrar = (Button) findViewById(R.id.buttonEntrar);
+        buttonCadastrarUsuario = (Button) findViewById(R.id.buttonCadastrarUsuario);
+
+        buttonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Menu.class);
+                startActivity(i);
+
+            }
+        });
+
+        buttonCadastrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CadastrarUsuario.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
