63be1972d3e5f02ca0b1fe832976e9fb50801d43
ramon <ramonespontes@gmail.com>
2019-06-13 22:05:14 -0300

    Tela de Menu

--- app/src/main/java/ifpb/edu/br/educamais/MainActivity.java
+++ app/src/main/java/ifpb/edu/br/educamais/MainActivity.java
@@ -1,11 +1,21 @@
 package ifpb.edu.br.educamais;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,8 +37,35 @@ public class MainActivity extends AppCompatActivity {
         buttonEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), Menu.class);
-                startActivity(i);
+
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "E-mail ou Senha devem ser preenchidos!!!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                try {
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Toast.makeText(getApplicationContext(), "Usuário autenticado!",Toast.LENGTH_LONG).show();
+                            Log.i("Autenticando", task.getResult().getUser().getUid());
+
+                            Intent i = new Intent(getApplicationContext(), Menu.class);
+                            startActivity(i);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                            Log.i("Falha na autenticação", e.getMessage());
+                        }
+                    });
+                }catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Usuário Não Cdastrado!",Toast.LENGTH_LONG).show();
+                }
 
             }
         });
