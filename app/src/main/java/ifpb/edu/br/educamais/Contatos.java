62b5225fd9cb38861501329d9d246044accad635
ramon <ramonespontes@gmail.com>
2019-06-19 18:24:06 -0300

    Alteração na tela de contatos circular

--- app/src/main/java/ifpb/edu/br/educamais/Contatos.java
+++ app/src/main/java/ifpb/edu/br/educamais/Contatos.java
@@ -1,15 +1,23 @@
 package ifpb.edu.br.educamais;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.Item;
+import com.xwray.groupie.ViewHolder;
 
 import java.util.List;
 
@@ -17,10 +25,18 @@ import javax.annotation.Nullable;
 
 public class Contatos extends AppCompatActivity {
 
+    private GroupAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contatos);
+
+        RecyclerView recyclerView = findViewById(R.id.reciclerListaContatos);
+
+         adapter = new GroupAdapter();
+         recyclerView.setAdapter(adapter);
+         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
         //Método que pega os usuários cadastrados no FireBase..
         buscarUsuariosFirebase();
@@ -36,9 +52,37 @@ public class Contatos extends AppCompatActivity {
                 List<DocumentSnapshot> docs = queryDocumentSnapshots.getDocuments();
                 for (DocumentSnapshot doc: docs) {
                     User usuario = doc.toObject(User.class);
-                    Log.d("usuario", usuario.getUsername());
+
+                    adapter.add(new UserItem(usuario));
+
+
                 }
             }
         });
+    }
+
+    private class UserItem extends Item<ViewHolder>{
+
+        private final User usuario;
+
+        private UserItem(User usuario) {
+            this.usuario = usuario;
+        }
+
+
+        @Override
+        public void bind(@NonNull ViewHolder viewHolder, int position) {
+            TextView textViewNomeContato = viewHolder.itemView.findViewById(R.id.textViewNomeContato);
+            ImageView imageViewFoto = viewHolder.itemView.findViewById(R.id.imageViewFoto);
+
+            textViewNomeContato.setText(usuario.getUsername());
+
+            Picasso.get().load(usuario.getProfileURL()).into(imageViewFoto);
+        }
+
+        @Override
+        public int getLayout() {
+            return R.layout.item_usuario;
+        }
     }
 }
